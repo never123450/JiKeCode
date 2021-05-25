c8f3d5c659594530112cf69554a4b2c0e9fdf2b2
845619585@qq.com <845619585@qq.com>
2021-05-25 22:03:56 +0800

    'src'

---
+++ src/main/java/com/xwy/nio/two/atomic/Count.java
@@ -0,0 +1,15 @@
+
+package com.xwy.nio.two.atomic;
+
+public class Count {
+
+    private int num = 0;
+
+    public int add() {
+        return num++;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
